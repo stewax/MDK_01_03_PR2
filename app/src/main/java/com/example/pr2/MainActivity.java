1454e65048904b061f5c5d083bf1d25cadca2633
qwelord <secondofall22@rambler.ru>
2026-02-24 14:54:36 +0500

    Разработанная практическая работа на оценку удовлетворительно

--- app/src/main/java/com/example/pr2/MainActivity.java
+++ app/src/main/java/com/example/pr2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.pr2;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,13 +20,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onStepAge(View view){
-        EditText _name = findViewById(R.id.editTextTextMultiline);
+        EditText tbName = findViewById(R.id.tbName);
+        String NameUser = tbName.getText().toString();
 
         setContentView(R.layout.step_2);
+
+        TextView tvName = findViewById(R.id.tvName);
+        tvName.setText("Очень приятно познакомиться " +NameUser+" Меня зовут Бисквит. \nСколько тебе лет?");
     }
 
     public void onStepSex(View view){
+        EditText tbAge = findViewById(R.id.tbAge);
+        String AgeUser = tbAge.getText().toString();
         setContentView(R.layout.step_3);
+        TextView tvAge = findViewById(R.id.tvAge);
+        tvAge.setText("Ничего себе ты большой, целых " +AgeUser+ " лет \n Какого ты пола?");
     }
 
     public void onStepEmail(View view){
